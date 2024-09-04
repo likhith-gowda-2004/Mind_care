97e04c7760ec0c83ede741f3c1d9f76389264c93
likhith-gowda-2004 <likhithgowda2018@gmail.com>
2024-09-04 22:40:45 +0530

    Initial commit with existing project files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navigation/Navbar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      
+    </div>
+  )
+}
+
+export default App
+
