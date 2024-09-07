24d346f2f3d7d6921a581225a3a7fefaaf77470f
Krishna <krishnakravi007@gmail.com>
2024-09-07 21:03:00 +0530

    basic react app and pages set up

---
+++ frontend/frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+// src/pages/Home.js
+import React from 'react';
+
+const Home = () => {
+  console.log("Home component rendered");
+  return (
+    <div>
+      <h1>Welcome to the Student Mental Health Support System</h1>
+      <p>Your well-being is our priority.</p>
+    </div>
+  );
+};
+
+export default Home;
